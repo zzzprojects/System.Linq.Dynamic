e036afb50acb4fb65767e62b358d763ef5065a79
Nathan Arnott <nathan.arnott@gmail.com>
2014-04-04 09:34:53 -0700

    Added XML Documentation to the primary DynamicQueryable Methods

--- Src/System.Linq.Dynamic/DynamicQueryable.cs
+++ Src/System.Linq.Dynamic/DynamicQueryable.cs
@@ -8,21 +8,53 @@ using System.Threading;
 namespace System.Linq.Dynamic
 {
     /// <summary>
-    /// Microsoft provided class. It allows dynamic string based querying. 
-    /// Very handy when, at compile time, you don't know the type of queries that will be generated.
+    /// Provides a set of static (Shared in Visual Basic) methods for querying data 
+    /// structures that implement <see cref="IQueryable"/>. It allows dynamic string based querying. 
+    /// Very handy when, at compile time, you don't know the type of queries that will be generated, 
+    /// or when downstream components only return column names to sort and filter by.
     /// </summary>
     public static class DynamicQueryable
     {
-        public static IQueryable<T> Where<T>(this IQueryable<T> source, string predicate, params object[] values)
+
+        /// <summary>
+        /// Filters a sequence of values based on a predicate.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">A <see cref="IQueryable{T}"/> to filter.</param>
+        /// <param name="predicate">An expression string to test each element for a condition.</param>
+        /// <param name="args">An object array that contains zero or more objects to insert into the predicate as parameters.  Similiar to the way String.Format formats strings.</param>
+        /// <returns>A <see cref="IQueryable{T}"/> that contains elements from the input sequence that satisfy the condition specified by predicate.</returns>
+        /// <example>
+        /// <code>
+        /// var result1 = list.Where("NumberProperty=1");
+        /// var result2 = list.Where("NumberProperty=@0", 1);
+        /// var result3 = list.Where("NumberProperty=@0", SomeIntValue);
+        /// </code>
+        /// </example>
+        public static IQueryable<TSource> Where<TSource>(this IQueryable<TSource> source, string predicate, params object[] args)
         {
-            return (IQueryable<T>)Where((IQueryable)source, predicate, values);
+            return (IQueryable<TSource>)Where((IQueryable)source, predicate, args);
         }
 
-        public static IQueryable Where(this IQueryable source, string predicate, params object[] values)
+        /// <summary>
+        /// Filters a sequence of values based on a predicate.
+        /// </summary>
+        /// <param name="source">A <see cref="IQueryable"/> to filter.</param>
+        /// <param name="predicate">An expression string to test each element for a condition.</param>
+        /// <param name="args">An object array that contains zero or more objects to insert into the predicate as parameters.  Similiar to the way String.Format formats strings.</param>
+        /// <returns>A <see cref="IQueryable"/> that contains elements from the input sequence that satisfy the condition specified by predicate.</returns>
+        /// <example>
+        /// <code>
+        /// var result1 = list.Where("NumberProperty=1");
+        /// var result2 = list.Where("NumberProperty=@0", 1);
+        /// var result3 = list.Where("NumberProperty=@0", SomeIntValue);
+        /// </code>
+        /// </example>
+        public static IQueryable Where(this IQueryable source, string predicate, params object[] args)
         {
             if (source == null) throw new ArgumentNullException("source");
             if (predicate == null) throw new ArgumentNullException("predicate");
-            LambdaExpression lambda = DynamicExpression.ParseLambda(source.ElementType, typeof(bool), predicate, values);
+            LambdaExpression lambda = DynamicExpression.ParseLambda(source.ElementType, typeof(bool), predicate, args);
             return source.Provider.CreateQuery(
                 Expression.Call(
                     typeof(Queryable), "Where",
@@ -30,11 +62,18 @@ namespace System.Linq.Dynamic
                     source.Expression, Expression.Quote(lambda)));
         }
 
-        public static IQueryable Select(this IQueryable source, string selector, params object[] values)
+        /// <summary>
+        /// Projects each element of a sequence into a new form.
+        /// </summary>
+        /// <param name="source">A sequence of values to project.</param>
+        /// <param name="selector">A projection string to apply to each element.</param>
+        /// <param name="args">An object array that contains zero or more objects to insert into the predicate as parameters.  Similiar to the way String.Format formats strings.</param>
+        /// <returns>An <see cref="IQueryable{T}"/> whose elements are the result of invoking a projection string on each element of source.</returns>
+        public static IQueryable Select(this IQueryable source, string selector, params object[] args)
         {
             if (source == null) throw new ArgumentNullException("source");
             if (selector == null) throw new ArgumentNullException("selector");
-            LambdaExpression lambda = DynamicExpression.ParseLambda(source.ElementType, null, selector, values);
+            LambdaExpression lambda = DynamicExpression.ParseLambda(source.ElementType, null, selector, args);
             return source.Provider.CreateQuery(
                 Expression.Call(
                     typeof(Queryable), "Select",
@@ -42,18 +81,43 @@ namespace System.Linq.Dynamic
                     source.Expression, Expression.Quote(lambda)));
         }
 
-        public static IQueryable<T> OrderBy<T>(this IQueryable<T> source, string ordering, params object[] values)
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending order according to a key.
+        /// </summary>
+        /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="ordering">An expression string to indicate values to order by.</param>
+        /// <param name="args">An object array that contains zero or more objects to insert into the predicate as parameters.  Similiar to the way String.Format formats strings.</param>
+        /// <returns>A <see cref="IQueryable{T}"/> whose elements are sorted according to the specified <paramref name="ordering"/>.</returns>
+        /// <example>
+        /// <code>
+        /// var result = list.OrderBy("NumberProperty, StringProperty DESC");
+        /// </code>
+        /// </example>
+        public static IQueryable<TSource> OrderBy<TSource>(this IQueryable<TSource> source, string ordering, params object[] args)
         {
-            return (IQueryable<T>)OrderBy((IQueryable)source, ordering, values);
+            return (IQueryable<TSource>)OrderBy((IQueryable)source, ordering, args);
         }
 
-        public static IQueryable OrderBy(this IQueryable source, string ordering, params object[] values)
+        /// <summary>
+        /// Sorts the elements of a sequence in ascending order according to a key.
+        /// </summary>
+        /// <param name="source">A sequence of values to order.</param>
+        /// <param name="ordering">An expression string to indicate values to order by.</param>
+        /// <param name="args">An object array that contains zero or more objects to insert into the predicate as parameters.  Similiar to the way String.Format formats strings.</param>
+        /// <returns>A <see cref="IQueryable"/> whose elements are sorted according to the specified <paramref name="ordering"/>.</returns>
+        /// <example>
+        /// <code>
+        /// var result = list.OrderBy("NumberProperty, StringProperty DESC");
+        /// </code>
+        /// </example>
+        public static IQueryable OrderBy(this IQueryable source, string ordering, params object[] args)
         {
             if (source == null) throw new ArgumentNullException("source");
             if (ordering == null) throw new ArgumentNullException("ordering");
             ParameterExpression[] parameters = new ParameterExpression[] {
                 Expression.Parameter(source.ElementType, "") };
-            ExpressionParser parser = new ExpressionParser(parameters, ordering, values);
+            ExpressionParser parser = new ExpressionParser(parameters, ordering, args);
             IEnumerable<DynamicOrdering> orderings = parser.ParseOrdering();
             Expression queryExpr = source.Expression;
             string methodAsc = "OrderBy";
@@ -70,6 +134,12 @@ namespace System.Linq.Dynamic
             return source.Provider.CreateQuery(queryExpr);
         }
 
+        /// <summary>
+        /// Returns a specified number of contiguous elements from the start of a sequence.
+        /// </summary>
+        /// <param name="source">The sequence to return elements from.</param>
+        /// <param name="count">The number of elements to return.</param>
+        /// <returns>A <see cref="IQueryable"/> that contains the specified number of elements from the start of source.</returns>
         public static IQueryable Take(this IQueryable source, int count)
         {
             if (source == null) throw new ArgumentNullException("source");
@@ -80,9 +150,16 @@ namespace System.Linq.Dynamic
                     source.Expression, Expression.Constant(count)));
         }
 
+        /// <summary>
+        /// Bypasses a specified number of elements in a sequence and then returns the remaining elements.
+        /// </summary>
+        /// <param name="source">A <see cref="IQueryable"/> to return elements from.</param>
+        /// <param name="count">The number of elements to skip before returning the remaining elements.</param>
+        /// <returns>A <see cref="IQueryable"/> that contains elements that occur after the specified index in the input sequence.</returns>
         public static IQueryable Skip(this IQueryable source, int count)
         {
             if (source == null) throw new ArgumentNullException("source");
+
             return source.Provider.CreateQuery(
                 Expression.Call(
                     typeof(Queryable), "Skip",
@@ -90,13 +167,23 @@ namespace System.Linq.Dynamic
                     source.Expression, Expression.Constant(count)));
         }
 
-        public static IQueryable GroupBy(this IQueryable source, string keySelector, string elementSelector, params object[] values)
+        /// <summary>
+        /// Groups the elements of a sequence according to a specified key string function 
+        /// and creates a result value from each group and its key.
+        /// </summary>
+        /// <param name="source">A <see cref="IQueryable"/> whose elements to group.</param>
+        /// <param name="keySelector">A string to specify the key for each element.</param>
+        /// <param name="resultSelector">A string to specify a result value from each group.</param>
+        /// <param name="args">An object array that contains zero or more objects to insert into the predicate as parameters.  Similiar to the way String.Format formats strings.</param>
+        /// <returns>A <see cref="IQueryable"/> where each element represents a projection over a group and its key.</returns>
+        public static IQueryable GroupBy(this IQueryable source, string keySelector, string resultSelector, params object[] args)
         {
             if (source == null) throw new ArgumentNullException("source");
             if (keySelector == null) throw new ArgumentNullException("keySelector");
-            if (elementSelector == null) throw new ArgumentNullException("elementSelector");
-            LambdaExpression keyLambda = DynamicExpression.ParseLambda(source.ElementType, null, keySelector, values);
-            LambdaExpression elementLambda = DynamicExpression.ParseLambda(source.ElementType, null, elementSelector, values);
+            if (resultSelector == null) throw new ArgumentNullException("elementSelector");
+
+            LambdaExpression keyLambda = DynamicExpression.ParseLambda(source.ElementType, null, keySelector, args);
+            LambdaExpression elementLambda = DynamicExpression.ParseLambda(source.ElementType, null, resultSelector, args);
             return source.Provider.CreateQuery(
                 Expression.Call(
                     typeof(Queryable), "GroupBy",
@@ -104,6 +191,11 @@ namespace System.Linq.Dynamic
                     source.Expression, Expression.Quote(keyLambda), Expression.Quote(elementLambda)));
         }
 
+        /// <summary>
+        /// Determines whether a sequence contains any elements.
+        /// </summary>
+        /// <param name="source">A sequence to check for being empty.</param>
+        /// <returns>true if the source sequence contains any elements; otherwise, false.</returns>
         public static bool Any(this IQueryable source)
         {
             if (source == null) throw new ArgumentNullException("source");
@@ -113,6 +205,11 @@ namespace System.Linq.Dynamic
                     new Type[] { source.ElementType }, source.Expression));
         }
 
+        /// <summary>
+        /// Returns the number of elements in a sequence.
+        /// </summary>
+        /// <param name="source">The <see cref="IQueryable"/> that contains the elements to be counted.</param>
+        /// <returns>The number of elements in the input sequence.</returns>
         public static int Count(this IQueryable source)
         {
             if (source == null) throw new ArgumentNullException("source");
