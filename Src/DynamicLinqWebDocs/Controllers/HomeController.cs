bc27c3a07b690c3d23d0041d4f365d9df44b730c
Nathan Arnott <nathan.arnott@gmail.com>
2014-06-11 08:59:24 -0700

    Updating several general pages' contents

--- Src/DynamicLinqWebDocs/Controllers/HomeController.cs
+++ Src/DynamicLinqWebDocs/Controllers/HomeController.cs
@@ -14,19 +14,9 @@ namespace DynamicLinqWebDocs.Controllers
             return View();
         }
 
-        [Route("About")]
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         [Route("Contact")]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ Src/DynamicLinqWebDocs/Infrastructure/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DynamicLinqWebDocs.Infrastructure
+{
+    public static class Settings
+    {
+        public const string GIT_HUB_LINK = "https://github.com/NArnott/System.Linq.Dynamic";
+        public const string GIT_HUB_ISSUES = GIT_HUB_LINK + "/issues";
+
+
+    }
+}
