0dabdee5babf5fcc1269cf020cb0c9837cf8a264
Nathan Arnott <nathan.arnott@gmail.com>
2014-06-11 10:20:56 -0700

    Removed modernizr package and adjusted js/css bundles

--- Src/DynamicLinqWebDocs/App_Start/BundleConfig.cs
+++ Src/DynamicLinqWebDocs/App_Start/BundleConfig.cs
@@ -8,18 +8,11 @@ namespace DynamicLinqWebDocs
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/sunlight-{version}.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                        "~/Scripts/sunlight-{version}.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
