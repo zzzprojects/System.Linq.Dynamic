821c4afd825db883c2b9b215c51e7d7d3db288d5
Nathan Arnott <nathan.arnott@gmail.com>
2014-06-16 16:00:18 -0700

    Added rejection of certain older browsers

--- Src/DynamicLinqWebDocs/App_Start/BundleConfig.cs
+++ Src/DynamicLinqWebDocs/App_Start/BundleConfig.cs
@@ -11,12 +11,15 @@ namespace DynamicLinqWebDocs
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/sunlight-{version}.js",
+                        "~/Scripts/jquery.reject.js",
                         "~/Scripts/bootstrap.js",
-                        "~/Scripts/respond.js"));
+                        "~/Scripts/respond.js",
+                        "~/Scripts/site.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/sunlight.default-{version}.css",
+                      "~/Content/jquery.reject.css",
                       "~/Content/site.css"));
         }
     }
---
+++ Src/DynamicLinqWebDocs/Scripts/site.js
@@ -0,0 +1,13 @@
+﻿//Highlight all code snippets
+Sunlight.highlightAll();
+
+//Check for and reject all unsupported browsers (http://jreject.turnwheel.com/)
+$.reject({
+    reject: {
+        msie: 9,    //IE:      Reject version 9 and below
+        chrome: 26, //Chrome:  Reject version 26 and below
+        firefox: 21 //Firefox: Reject version 21 and below
+    },
+    close: false, //disable close option
+    display: ['msie', 'chrome', 'firefox', 'safari'] //set browsers to display and their order
+});
