450ef25f4b54f75290658b066f4e81d4f9bd9188
Nathan Arnott <nathan.arnott@gmail.com>
2014-07-15 08:14:22 -0700

    Added Google Custom Search

--- Src/DynamicLinqWebDocs/Scripts/site.js
+++ Src/DynamicLinqWebDocs/Scripts/site.js
@@ -11,3 +11,15 @@ $.reject({
     close: false, //disable close option
     display: ['msie', 'chrome', 'firefox', 'safari'] //set browsers to display and their order
 });
+
+//Custom Google Search Code
+(function () {
+    var cx = '009560997830204618670:m5_ucxqmsyw';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
