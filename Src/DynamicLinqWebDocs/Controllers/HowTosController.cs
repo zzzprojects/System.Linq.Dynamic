4d9b693f5f33bf1a36163f0eca5454c596a2b5ce
Nathan Arnott <nathan.arnott@gmail.com>
2015-04-06 11:23:40 -0700

    Added How Tos to Doc Site with first how to

---
+++ Src/DynamicLinqWebDocs/Controllers/HowTosController.cs
@@ -0,0 +1,36 @@
+﻿using DynamicLinqWebDocs.Infrastructure;
+using SimpleMvcSitemap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicLinqWebDocs.Controllers
+{
+    [RoutePrefix("HowTos")]
+    public class HowTosController : Controller
+    {
+        [Route()]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("EF6")]
+        // GET: HowTos
+        public ActionResult EF6()
+        {
+            return View();
+        }
+
+        class HowTosSitemap : SitemapContributor
+        {
+            protected internal override IEnumerable<SitemapNode> GetSitemapNodes(UrlHelper urlHelper, HttpContextBase httpContext)
+            {
+                yield return new SitemapNode(urlHelper.Action("Index", "HowTos")) { Priority = 0.3m };
+                yield return new SitemapNode(urlHelper.Action("EF6", "HowTos")) { Priority = 0.4m };
+            }
+        }
+    }
+}
