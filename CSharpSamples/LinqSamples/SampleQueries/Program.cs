31aedf15be510e895d09738b9461a9fb6e7de47e
Ryan Gates <ryanvgates@gmail.com>
2015-09-21 13:28:01 -0400

    Added the Sample Queries project to the repository under CSharpSamples/LinqSamples

---
+++ CSharpSamples/LinqSamples/SampleQueries/Program.cs
@@ -0,0 +1,60 @@
+﻿//Copyright (C) Microsoft Corporation.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using SampleSupport;
+using SampleQueries;
+using System.IO;
+using DataSetSampleQueries;
+
+// See the ReadMe.html for additional information
+namespace SampleQueries
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            List<SampleHarness> harnesses = new List<SampleHarness>();
+
+            // LinqSamples:
+            LinqSamples linqHarness = new LinqSamples();
+            harnesses.Add(linqHarness);
+
+            // Linq To SQL Samples:
+            LinqToSqlSamples linqToSqlHarness = new LinqToSqlSamples();
+            harnesses.Add(linqToSqlHarness);
+
+            // LinqToXmlSamples:
+            LinqToXmlSamples linqToXmlHarness = new LinqToXmlSamples();
+            harnesses.Add(linqToXmlHarness);
+
+            // DataSetLinqSamples:
+            DataSetLinqSamples dsLinqSamples = new DataSetLinqSamples();
+            harnesses.Add(dsLinqSamples);
+            
+            // XQueryUseCases:
+            XQueryUseCases xqueryHarness = new XQueryUseCases();
+            harnesses.Add(xqueryHarness);
+
+            if (args.Length >= 1 && args[0] == "/runall") {
+                foreach (SampleHarness harness in harnesses)
+                {
+                    harness.RunAllSamples();
+                }
+            }
+            else {
+                Application.EnableVisualStyles();
+                
+                using (SampleForm form = new SampleForm("LINQ Project Sample Query Explorer", harnesses))
+                {
+                    form.ShowDialog();
+                }
+            }
+        }
+    }
+}
