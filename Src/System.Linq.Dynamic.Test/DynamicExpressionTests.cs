9438e8fa0304fc8cc8022c0d29241504185b9bcb
Billy Ho <billyho@thisisnoble.com>
2016-03-25 09:27:30 +0800

    Add unit test case CreateClass_ThreadSafe to verify the fix of the multiple threading issue in ClassFactory.GetDynamicClass

--- Src/System.Linq.Dynamic.Test/DynamicExpressionTests.cs
+++ Src/System.Linq.Dynamic.Test/DynamicExpressionTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace System.Linq.Dynamic.Test
@@ -14,6 +16,23 @@ namespace System.Linq.Dynamic.Test
                 typeof(string),
                 "it.ToString()");
             Assert.AreEqual(typeof(string), expression.ReturnType);
+        }
+
+        [TestMethod]
+        public void CreateClass_TheadSafe()
+        {
+            const int numOfTasks = 15;
+
+            var properties = new[] { new DynamicProperty("prop1", typeof(string)) };
+
+            var tasks = new List<Task>(numOfTasks);
+
+            for (var i = 0; i < numOfTasks; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() => DynamicExpression.CreateClass(properties)));
+            }
+
+            Task.WaitAll(tasks.ToArray());
         }
     }
 }
