27116c638d9e48d0c1a8a07f31d19f38ab6d5547
meatGUY <karasik.oleg.nikolaevich@gmail.com>
2017-06-04 00:13:53 +0300

    Updated linq to NuGet package.

--- Src/DynamicLinqWebDocs/Infrastructure/Settings.cs
+++ Src/DynamicLinqWebDocs/Infrastructure/Settings.cs
@@ -12,7 +12,7 @@ namespace DynamicLinqWebDocs.Infrastructure
         public const string GIT_HUB_LOGO = "/Content/github-logo.png";
         public const string GIT_HUB_LOGO_TEXT = "/Content/github-logo-text.png";
         
-        public const string NUGET_LINK = "https://www.nuget.org/packages/System.Linq.Dynamic.Library/";
+        public const string NUGET_LINK = "https://www.nuget.org/packages/AK.System.Linq.Dynamic/";
         public const string NUGET_LOGO = "https://www.nuget.org/Content/Logos/nugetlogo.png";
 
         public const string LINKEDIN_LOGO = "/Content/linkedin-logo.png";
