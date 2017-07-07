38edc780c11468971232c61f4433bdc1d2787661
Aleh Karasik <karasik.oleg.nikolaevich@gmail.com>
2017-07-07 16:05:22 +0300

    Improved formatting of all content fron XML (Markdown, Examples, Remarks).
    Improved code style for HtmlHelpers methods.

--- Src/DynamicLinqWebDocs/Infrastructure/HtmlHelpers.cs
+++ Src/DynamicLinqWebDocs/Infrastructure/HtmlHelpers.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
@@ -10,19 +9,9 @@ namespace DynamicLinqWebDocs.Infrastructure
 {
     public static class HtmlHelpers
     {
-        public static HtmlString FormatMarkdown(this HtmlHelper helper, string value)
+        private static string FormatText(string value)
         {
-            return FormatMarkdown(value);
-        }
-
-        public static HtmlString FormatMarkdown(this HtmlHelper helper, string format, params object[] args)
-        {
-            return FormatMarkdown(String.Format(format, args));
-        }
-
-        public static HtmlString FormatCodeBlock(this HtmlHelper helper, string value)
-        {
-            if (String.IsNullOrWhiteSpace(value)) return null;
+            if (string.IsNullOrWhiteSpace(value)) return null;
 
             var lines = value.Split(new[] { '\n' }, StringSplitOptions.None);
 
@@ -72,66 +61,58 @@ namespace DynamicLinqWebDocs.Infrastructure
                 value = value.TrimStart();
             }
 
-            var pre = new TagBuilder("pre");
-
-            pre.AddCssClass("sunlight-highlight-csharp");
-
-            pre.SetInnerText(value);
-
-            return new HtmlString(pre.ToString());
+            return value;
         }
 
-        static HtmlString FormatMarkdown(string value)
+        private static HtmlString FormatMarkdown(string value)
         {
             if (String.IsNullOrWhiteSpace(value)) return null;
 
             var md = new MarkdownSharp.Markdown();
 
-            //if (convertToCode) value = ConvertToCode(value);
-
-            var result = md.Transform(value);
+            var result = md.Transform(FormatText(value));
 
             return new HtmlString(result);
         }
 
-        public static HtmlString InlineCodeList(this HtmlHelper helper, params string[] items)
+        public static HtmlString FormatMarkdown(this HtmlHelper helper, string value)
+        {
+            return FormatMarkdown(value);
+        }
+
+        public static HtmlString FormatMarkdown(this HtmlHelper helper, string format, params object[] args)
+        {
+            return FormatMarkdown(String.Format(format, args));
+        }
+
+        public static HtmlString FormatCodeBlock(this HtmlHelper helper, string value)
+        {
+            if (String.IsNullOrWhiteSpace(value)) return null;
+
+            var pre = new TagBuilder("pre");
+
+            pre.AddCssClass("sunlight-highlight-csharp");
+
+            pre.SetInnerText(FormatText(value));
+
+            return new HtmlString(pre.ToString());
+        }
+
+        public static HtmlString FormatInlineCodeList(this HtmlHelper helper, params string[] items)
         {
             var ul = new TagBuilder("ul");
             ul.AddCssClass("list-inline");
 
             var sb = new StringBuilder();
 
-            foreach(var item in items)
+            foreach (var item in items)
             {
-                sb.AppendFormat("<li><code>{0}</code></li>", item);
+                sb.AppendFormat("<li><code>{0}</code></li>", FormatText(item));
             }
 
             ul.InnerHtml = sb.ToString();
 
             return new HtmlString(ul.ToString());
-        }
-
-
-        public static IDisposable BeginNote(this HtmlHelper helper)
-        {
-            return new HtmlBootstrapNote(helper.ViewContext);
-        }
-
-        class HtmlBootstrapNote : IDisposable
-        {
-            TextWriter _writer;
-
-            public HtmlBootstrapNote(ViewContext context)
-            {
-                _writer = context.Writer;
-
-                _writer.WriteLine("<div class=\"panel panel-default\"><div class=\"panel-heading\">Note</div><div class=\"panel-body\">");
-            }
-
-            public void Dispose()
-            {
-                _writer.WriteLine("</div></div>");
-            }
         }
 
         public static HtmlString IsActive(this HtmlHelper html, string url)
@@ -157,25 +138,14 @@ namespace DynamicLinqWebDocs.Infrastructure
             return isActive ? new HtmlString("active") : null;
         }
 
-        //static string ConvertToCode(string value)
-        //{
-        //    var sb = new StringBuilder();
+        public static HtmlString OpenNote(this HtmlHelper helper)
+        {
+            return new HtmlString("<div class=\"panel panel-default\"><div class=\"panel-heading\">Note</div><div class=\"panel-body\">");
+        }
 
-        //    if (value.Length > 0) sb.Append("    ");
-
-        //    for( int i = 0; i < value.Length; i++)
-        //    {
-        //        sb.Append(value[i]);
-
-        //        //only add space if we're not at the end
-        //        if (value[i] == '\n' && i != value.Length - 1)
-        //        {
-        //            sb.Append("    ");
-        //        }
-        //    }
-
-        //    return sb.ToString();
-        //}
-
+        public static HtmlString CloseNote(this HtmlHelper helper)
+        {
+            return new HtmlString("</div></div>");
+        }
     }
 }
